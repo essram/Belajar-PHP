f8cdd50d5b38e6de36b22d233a14c3dc07bda19e
essram <esarama22@gmail.com>
2024-03-26 21:28:01 +0700

    Tipe Data Boolean

---
+++ tipedata/intFloat.php
@@ -0,0 +1,17 @@
+<?php 
+$a = 10;
+$b = 3;
+$c = $a / $b;
+echo round ($c, 2) . "<br>"; //membulatkan $c;
+
+//operator increment
+$z = 10;
+$z++;
+echo $z . "<br>";
+
+//operator increment
+$y = 10;
+$y--;
+echo $y . "<br>";
+?>
+
---
+++ tipedata/string.php
@@ -0,0 +1,11 @@
+<?php 
+//cara pertama
+$nama = "Budi";
+$pesan = "Selamat Datang, $nama";
+echo $pesan . "<br>";
+
+//cara kedua
+$nama = "asep";
+$pesan = "Selamat Datang";
+echo $pesan . ', '.$nama;
+?>
