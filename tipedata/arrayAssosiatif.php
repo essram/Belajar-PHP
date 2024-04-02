3f9fb8f19da71adf0305c1db101486b78cb21d6a
essram <esarama22@gmail.com>
2024-04-02 22:47:22 +0700

    Array Assosiatif

--- tipedata/arrayAssosiatif.php
+++ tipedata/arrayAssosiatif.php
@@ -1,7 +1,28 @@
 <?php 
 // $a = ["sapi", "ayam", "kambing"]; CARA PERTAMA
-$a[] = "sapi";
-$a[] = "ayam";
-$a[] = "kambing";
-var_dump($a);
+
+//index menggunakan angka
+// $a[10] = "sapi";
+// $a[20] = "ayam";
+// $a[30] = "kambing";
+// var_dump($a);
+
+//index menggunakan String
+// $dataDiri["nama"] = "Rama Esa";
+// $dataDiri["golDarah"] = "O";
+// $dataDiri["alamat"] = "Malang";
+
+//cara lain
+$dataDiri = [
+    "nama" => "Rama Esa",
+    "golDarah" => "O",
+    "alamat" => "Malang"
+];
+
+
+echo "Nama : ".$dataDiri["nama"]."<br>";
+echo "Gol. Darah : ".$dataDiri["golDarah"]."<br>";
+echo "Alamat : ".$dataDiri["alamat"]."<br>";
+
+
 ?>
