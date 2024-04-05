692c3e2c6027c4a2d8631657b3c20f6224ac04dc
essram <esarama22@gmail.com>
2024-04-05 20:55:57 +0700

    Method POST pada form PHP

---
+++ Form/POST/POST.php
@@ -0,0 +1,15 @@
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Formulir Pengguna</title>
+    </head>
+    <body>
+        <form action="proses.php" method="POST">
+            Nama: <input type="text" name="nama"><br>
+            Alamat: <input type="text" name="alamat"><br>
+            <input type="submit" value="Kirim">
+        </form>
+    </body>
+    </html>
