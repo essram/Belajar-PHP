84f6ced60e87f6db5a17ca5f549300a2be182a9e
essram <esarama22@gmail.com>
2024-04-06 22:48:29 +0700

    If-Else pada PHP

---
+++ ifElse/index.php
@@ -0,0 +1,16 @@
+
+<!-- // $nama = 'hehe';
+// if ($nama == 'rama') {
+//     echo 'hello rama';
+// } else{
+//     echo 'hello user';
+// }  -->
+
+<?php 
+    $nilai = 50;
+    if ($nilai >= 50) {
+        echo 'selamat anda lulus, nilai anda' . $nilai;
+    } else {
+        echo 'anda tidak lulus';
+    }
+?>
