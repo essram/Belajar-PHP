7d4c3f174051f108b47fd35d3c0fdf0aebbaef9a
essram <esarama22@gmail.com>
2024-04-06 23:06:50 +0700

    If-Elseif-Else

--- ifElse/index.php
+++ ifElse/index.php
@@ -7,10 +7,16 @@
 // }  -->
 
 <?php 
-    $nilai = 50;
-    if ($nilai >= 50) {
-        echo 'selamat anda lulus, nilai anda' . $nilai;
+    $nilai = 90;
+    if($nilai >= 80){
+        echo 'A';
+    } else if($nilai >= 70){
+        echo 'B';
+    } else if($nilai >= 60){
+        echo 'C';
+    } else if($nilai >= 50){
+        echo 'D';
     } else {
-        echo 'anda tidak lulus';
+        echo 'E';
     }
 ?>
