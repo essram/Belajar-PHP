849fa45dadf54fe33f3510412ecf4ffd3789072c
essram <esarama22@gmail.com>
2024-04-13 22:23:10 +0700

    Operator Ternary

---
+++ Switch/index3.php
@@ -0,0 +1,20 @@
+<!-- Operator Ternary -->
+
+<?php 
+
+// Statement setelah tanda tanya "?" adalah 
+// statement untuk kondisi TRUE, sedangkan 
+// statement setelah titik dua ":" adalah 
+// statement untuk kondisi FALSE.
+
+$nilai = 70;
+
+echo ($nilai >= 80 ) ?"Anda lulus":"Anda tidak lulus";
+
+
+
+
+
+
+
+?>
