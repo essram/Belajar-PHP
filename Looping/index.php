8c40f1eb94584f4cd2971bd7d1e811bcd11beff4
essram <esarama22@gmail.com>
2024-04-14 21:53:59 +0700

    Penggunaan For pada Array

--- Looping/index.php
+++ Looping/index.php
@@ -1,6 +1,20 @@
 <?php 
+//for biasa
+// for($angka = 0; $angka <= 10; $angka++){
+//     echo $angka.", ";
+//}
 
-for($angka = 0; $angka <= 10; $angka++){
-    echo $angka."<br>";
+//for pada array
+$warna = ["merah","biru","kuning","merah","hijau","oranye","ungu",
+"biru", "kuning","merah","hijau","oranye","ungu","merah"];
+
+$jumlah_merah=0;
+
+for($i = 0; $i<14;$i++){
+    if($warna[$i] == "merah"){
+        $jumlah_merah++;
 }
+}
+
+echo "jumlah warna merah = ".$jumlah_merah;
 ?>
