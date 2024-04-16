87441587df130d44dac1454ef590baffce4aac28
essram <esarama22@gmail.com>
2024-04-16 20:22:44 +0700

    Looping Foreach

---
+++ Looping/foreach.php
@@ -0,0 +1,11 @@
+<?php 
+
+$warna = ["merah","biru","kuning","merah","hijau","oranye","ungu",
+"biru", "kuning","merah","hijau","oranye","ungu","merah"];
+
+$jumlah_merah=0;
+
+foreach ()
+
+echo "jumlah warna merah = ".$jumlah_merah;
+?>
