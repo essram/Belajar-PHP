3ac34da6522d1e3b561f9d8e01844e048d7d1a1d
essram <esarama22@gmail.com>
2024-04-18 22:31:50 +0700

    Foreach

--- Looping/foreach.php
+++ Looping/foreach.php
@@ -5,7 +5,13 @@ $warna = ["merah","biru","kuning","merah","hijau","oranye","ungu",
 
 $jumlah_merah=0;
 
-foreach ()
+foreach ($warna as $nomor => $isi) {
+    echo "Index ke $nomor berisi $isi <br>";
+
+    if ($isi == "merah") {
+        $jumlah_merah++;
+    }
+}
 
 echo "jumlah warna merah = ".$jumlah_merah;
 ?>
